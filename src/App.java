03f6fdfc99f19fa72042faddb834fd55b4f70b19
droldan-2019310 <droldan-2019310@kinal.edu.gt>
2023-03-08 20:44:31 -0600

    factory implementation

--- src/App.java
+++ src/App.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
 
+import Interfaces.IMap;
+import factory.Factory;
+
 public class App {
 
 
     Scanner in  = new Scanner(System.in);
+    
+    Factory factory = new Factory();
+    IMap products;
     public static void main(String[] args) throws Exception {
         App app = new App();
         app.start();
@@ -11,10 +17,22 @@ public class App {
 
 
     public void start(){
-
+        menuMap();
     }
 
     public void menuMap(){
+        System.out.println("BIENVENIDO");
+        System.out.println("QUÉ MAP DESEAS USAR?");
+        System.out.println("1. HASHMAP");
+        System.out.println("2. LINKEDHASHMAP ");
+        System.out.println("3. TREEMAP");
+        int resp = in.nextInt();
 
+        products = factory.getMap(resp);
+    }
+
+
+    public void menuNormal(){
+      
     }
 }
